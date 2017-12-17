281e9b06509f0966de9d6afa2c6b6712de9bde33
Miles McCain <milesmcc@users.noreply.github.com>
2017-12-17 12:16:58 -0500

    Add footer, escape strings

--- popup/extract.js
+++ popup/extract.js
@@ -1,12 +1,19 @@
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+}
+
 function handleGraph(graph) {
   if (Object.keys(graph).length == 0) {
     document.getElementById("popup-content").innerHTML = "<p><i>No open graph data found.</i></p>";
   } else {
     html = "<table>";
+    var first = true;
     for (var key in graph) {
       html += "<tr style='padding-top: 2px;'>";
-      html += "<th style='text-align: left;'><small>" + key + "</small></th><th>&nbsp;</th>";
-      html += "<td><code>" + graph[key] + "</code></td>";
+      html += "<th style='text-align: left;'><small>" + escapeHtml(key) + "</small></th><th>&nbsp;</th>";
+      html += "<td><code>" + escapeHtml(graph[key]) + "</code></td>";
       html += "</tr>";
     }
     html += "</table>";
